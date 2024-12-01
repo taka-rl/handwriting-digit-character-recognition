a9951be134eee429dd48c287774c0da998687cb0
taka-rl <takaya.murakami.uni@gmail.com>
2024-12-01 15:52:38 +0100

    added comments and functions to display the prediction result including the tested image and the bar chart.

--- tf_practice/tensorflow_basics.py
+++ tf_practice/tensorflow_basics.py
@@ -1,79 +1,104 @@
-"""
-・links
-https://www.youtube.com/watch?v=eU0FFjYumCI
-https://www.kaggle.com/code/amyjang/tensorflow-mnist-cnn-tutorial
-https://www.tensorflow.org/datasets/keras_example
-
-"""
-
-
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
-import matplotlib.pyplot as plt
+from tensorflow.keras import layers
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-# Load MNIST for digit recognition
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+def plot_image(idx: int) -> None:
+    prediction, true_label, img = np.argmax(predictions[idx]), y_test[idx], x_test[idx]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(x_test[idx], cmap='gray')
 
-# Normalization
-x_train = x_train / 255
-x_test = x_test / 255
+    color = 'blue' if prediction == true_label else 'red'  # blue if correct, otherwise, red
 
-print(x_train.shape)
-print(x_test.shape)
-print(y_train.shape)
-
-# plt.imshow(x_train[0], cmap='Greys')
-# plt.show()
-
-#
-model_lr = tf.keras.models.Sequential([
-    layers.Input(x_train.shape[1:]),
-    layers.Flatten(),
-    layers.Dense(10, activation='softmax')
-])
-model_lr.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model_lr.summary()
-
-model_mlp = tf.keras.models.Sequential([
-    layers.Input(x_train.shape[1:]),
-    layers.Flatten(),
-    layers.Dense(64, activation='elu'),
-    layers.Dense(64, activation='elu'),
-    layers.Dense(10, activation='softmax')
-])
-model_mlp.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model_mlp.summary()
-
-# Train the model
-# y_onehot_train = tf.one_hot(y_train, 10)  # if you use loss='categorical_crossentropy
-history_lr = model_lr.fit(x_train, y_train, epochs=10, batch_size=128, validation_data=[x_test, y_test], verbose=False)  # validation_data=[x_test, y_test] or validation_split=0.2
-
-print(history_lr.history)
-
-plt.figure(1)
-plt.plot(history_lr.history['loss'], label='train')
-plt.plot(history_lr.history['val_loss'], label='val')
-plt.ylabel('loss')
-plt.legend()
-
-plt.figure(2)
-plt.plot(history_lr.history['accuracy'], label='train')
-plt.plot(history_lr.history['val_accuracy'], label='val')
-plt.ylabel('accuracy')
-plt.legend()
-plt.show()
-
-# evaluation
-model_lr.evaluate(x_test, y_test)
-
-probs = model_lr.predict(x_test[:5])
-preds = np.argmax(probs, axis=1)
-for i in range(0, 5):
-    print(probs[i], " => ", preds[i])
-    plt.imshow(x_test[i], cmap="Greys")
-    plt.figure(i)
-plt.show()
+    plt.xlabel(f'pred:{prediction}, {100*np.max(predictions[idx]):.2f}% (true:{true_label}', color=color)
 
 
+def plot_value_array(idx: int) -> None:
+    predictions_array, true_label = predictions[idx], y_test[idx]
+    plt.grid(False)
+    plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    plt.yticks([])
+    plot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    plot[predicted_label].set_color('red')
+    plot[true_label].set_color('blue')
+
+
+if __name__ == '__main__':
+
+    # Load MNIST for digit recognition
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    # Normalization
+    x_train, x_test = x_train / 255, x_test / 255
+
+    # Build a model
+    model_lr = tf.keras.models.Sequential([
+        layers.Input(x_train.shape[1:]),
+        layers.Flatten(),
+        layers.Dense(10, activation='softmax')
+    ])
+
+    # Compile the model
+    model_lr.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # Show the summary of the model
+    model_lr.summary()
+
+    model_mlp = tf.keras.models.Sequential([
+        layers.Input(x_train.shape[1:]),
+        layers.Flatten(),
+        layers.Dense(64, activation='elu'),
+        layers.Dense(64, activation='elu'),
+        layers.Dense(10, activation='softmax')
+    ])
+
+    # Compile the model
+    model_mlp.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # Show the summary of the model
+    model_mlp.summary()
+
+    # Train the model
+    # y_onehot_train = tf.one_hot(y_train, 10)  # if you use loss='categorical_crossentropy
+    # validation_data=[x_test, y_test] or validation_split=0.2
+    history_lr = model_lr.fit(x_train,
+                              y_train,
+                              epochs=10,
+                              batch_size=128,
+                              validation_data=[x_test, y_test],
+                              verbose=False)
+
+    print(history_lr.history)
+
+    plt.figure(1)
+    plt.plot(history_lr.history['loss'], label='train')
+    plt.plot(history_lr.history['val_loss'], label='val')
+    plt.ylabel('loss')
+    plt.legend()
+
+    plt.figure(2)
+    plt.plot(history_lr.history['accuracy'], label='train')
+    plt.plot(history_lr.history['val_accuracy'], label='val')
+    plt.ylabel('accuracy')
+    plt.legend()
+    plt.show()
+
+    # evaluation
+    model_lr.evaluate(x_test, y_test)
+
+    # Display the prediction result
+    predictions = model_lr.predict(x_test)
+    i: int = 12
+    plt.figure(figsize=(6, 3))
+    plt.subplot(1, 2, 1)
+    plot_image(i)
+    plt.subplot(1, 2, 2)
+    plot_value_array(i)
+    plt.show()
