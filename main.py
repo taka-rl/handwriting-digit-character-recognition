97ee5fd1c136929e659e661cfee47a815b4e6a4e
taka-rl <takaya.murakami.uni@gmail.com>
2024-12-11 12:18:46 +0100

    added a function to recognize a drawn digit on canvas.html

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify, render_template
 from tensorflow.keras import models
 from PIL import Image
 import numpy as np
+import base64
+import io
 
 
 app = Flask(__name__)
@@ -17,6 +19,42 @@ def canvas():
     return render_template('canvas.html')
 
 
+@app.route('/submit', methods=['POST'])
+def submit_drawing():
+    data = request.json.get('image', None)
+    if data is None:
+        return jsonify({"error": "No image data provided"})
+    try:
+        if ',' in data:
+            header, encoded = data.split(',', 1)
+            print(header)
+            print(encoded)
+        else:
+            return jsonify({'error': 'Invalid Base64 image format'}), 400
+
+        # Decode the Base 64 image
+        image_data = base64.b64decode(encoded)
+
+        # Preprocess the image
+        image = Image.open(io.BytesIO(image_data)).convert('L')
+        processed_image = preprocess_image(image)
+
+        # Predict the image
+        predictions = predict_digit(processed_image)
+
+        # Extract the predicted class and confidence
+        predicted_class = int(np.argmax(predictions))  # Convert NumPy scalar to int
+        confidence = float(np.max(predictions))  # Convert Numpy scalar to float
+
+        return jsonify({"prediction": predicted_class,
+                        "confidence": confidence,
+                        "probabilities": predictions.tolist()
+                        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
 @app.route('/import')
 def import_picture():
     return render_template('import.html')
@@ -28,10 +66,11 @@ def upload_picture():
     if file:
         print("Image is loaded")
         # Prepare the prediction
-        resized_image = preprocess_image(file)
+        image = Image.open(file).convert('L')  # Convert to grayscale
+        processed_image = preprocess_image(image)
 
         # Predict the image
-        predictions = predict_digit(resized_image)
+        predictions = predict_digit(processed_image)
 
         # Extract the predicted class and confidence
         predicted_class = int(np.argmax(predictions))  # Convert NumPy scalar to int
@@ -45,9 +84,8 @@ def upload_picture():
         return jsonify({"error": "No file uploaded"}), 400
 
 
-def preprocess_image(file, target_size=(28, 28)):
-    """Resize the image"""
-    image = Image.open(file).convert('L')  # Convert to grayscale
+def preprocess_image(image, target_size=(28, 28)):
+    """Resize and normalize the image."""
     img = image.resize(target_size)  # Resize image
     img_array = np.array(img) / 255.0  # Convert to numpy array and normalize
     return img_array[np.newaxis, :, :]  # Add batch dimension
