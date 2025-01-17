27fb386290723e17ad20a655dc5765f4e4e2e2a9
taka-rl <takaya.murakami.uni@gmail.com>
2025-01-17 21:27:09 +0100

    debug mode is off

--- main.py
+++ main.py
@@ -189,4 +189,4 @@ if __name__ == '__main__':
                       'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                       'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
-    app.run(debug=True)
+    app.run()
